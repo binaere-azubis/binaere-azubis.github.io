342e1bfb6edc6f0c0c8d62a9715662267d87d7dd
Dominik Kormann <dominikormann@outlook.com>
2023-08-06 20:05:26 +0200

    init

---
+++ app.js
@@ -0,0 +1,17 @@
+const app = Vue.createApp({
+
+    data(){
+        return {
+            
+            members:[
+                "dima","tongi","justus","kayal"
+            ],
+
+
+        }
+    },
+
+});
+
+app.mount('#app')
+
